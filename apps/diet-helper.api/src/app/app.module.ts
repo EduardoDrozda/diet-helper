bc872304c202f59915f9df86a63413eb32ab0795
Eduardo Drozda <Edu_Drozda@hotmail.com>
2024-07-14 23:20:15 -0300

    feat: config knexfile

--- apps/diet-helper.api/src/app/app.module.ts
+++ apps/diet-helper.api/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { join } from 'path';
-import * as Joi from 'joi';
+import Joi from 'joi';
 @Module({
   imports: [ConfigModule.forRoot({
     envFilePath: [
---
+++ knexfile.ts
@@ -0,0 +1,5 @@
+import { config } from './apps/diet-helper.api/src/app/database';
+
+module.exports = {
+  ...config,
+};
